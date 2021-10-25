aaf1bf99acf3c54a18847ef2cb86f9a70cb8897a
eduxxa <karaopap@rambler.ru>
2021-10-25 17:51:53 +0300

    up-2-date

--- src/main/java/com/example/workflow/TestService.java
+++ src/main/java/com/example/workflow/TestService.java
@@ -19,17 +19,13 @@ public class TestService implements JavaDelegate {
     @Override
     public void execute(DelegateExecution execution) throws Exception {
 
-            var id1 = execution.getVariable("email-adress").toString();
-            var id2 = execution.getVariable("email-text").toString();
+            var email = execution.getVariable("email-adress").toString();
+            var text = execution.getVariable("email-text").toString();
+
             final String username = "balkanabubakirov@gmail.com";
             final String password = "PisaPopa228";
 
             Properties prop = new Properties();
-            prop.put("mail.smtp.host", "smtp.gmail.com");
-            prop.put("mail.smtp.port", "465");
-            prop.put("mail.smtp.auth", "true");
-            prop.put("mail.smtp.socketFactory.port", "465");
-            prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
             Session session = Session.getInstance(prop,
                     new javax.mail.Authenticator() {
@@ -39,23 +35,20 @@ public class TestService implements JavaDelegate {
                     });
 
             try {
-
                 Message message = new MimeMessage(session);
                 message.setFrom(new InternetAddress("balkanabubakirov@gmail.com"));
                 message.setRecipients(
                         Message.RecipientType.TO,
-                        InternetAddress.parse(id1)
+                        InternetAddress.parse(email)
                 );
                 message.setSubject("Тестовое сообщение");
-                message.setText(id2);
-
+                message.setText(text);
                 Transport.send(message);
-
                 System.out.println("Сообщение отправлено!");
 
             } catch (MessagingException e) {
                 e.printStackTrace();
-                System.out.println("Сообщение отправлено!");
+                System.out.println("Сообщение не отправлено!");
             }
     }
 }
--- src/main/java/com/example/workflow/config/MailConfig.java
+++ src/main/java/com/example/workflow/config/MailConfig.java
@@ -1,15 +1,10 @@
-/*package com.example.workflow.config;
+package com.example.workflow.config;
 
 import org.springframework.stereotype.Component;
-
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
 import java.util.Properties;
 
 @Component
-public class MailConfig{
-    final String username = "balkanabubakirov@gmail.com";
-    final String password = "PisaPopa228";
+public class MailConfig {
     public void MailProp() {
         Properties prop = new Properties();
         prop.put("mail.smtp.host", "smtp.gmail.com");
@@ -18,11 +13,5 @@ public class MailConfig{
         prop.put("mail.smtp.socketFactory.port", "465");
         prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
-        Session session = Session.getInstance(prop,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                });
     }
-}*/
+}
